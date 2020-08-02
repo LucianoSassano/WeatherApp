4c40f8745f568d8de2685d6c67409ce27670d623
LucianoSassano <54543892+LucianoSassano@users.noreply.github.com>
2020-08-02 19:13:48 -0300

    favFeatureDone

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,6 +2,18 @@ import React from "react";
 import "../Weather/Weather.css";
 
 const Weather = (props) => {
+  const handleDelete = () => {
+    const localStorageFavs = JSON.parse(localStorage.getItem("favorites"));
+    let jsonFavs = [];
+    if (Array.isArray(localStorageFavs)) jsonFavs = localStorageFavs;
+    jsonFavs = jsonFavs.filter(
+      (weather) =>
+        weather.city != props.city && weather.country != props.country
+    );
+    console.log(jsonFavs);
+    localStorage.setItem("favorites", JSON.stringify(jsonFavs));
+  };
+
   return (
     <>
       <div className="weather-container">
@@ -12,6 +24,9 @@ const Weather = (props) => {
             </h1>
             <h3>{props.temp} °C</h3>
             <h2>{props.description}</h2>
+            <button className="btn btn-danger" onClick={handleDelete}>
+              Delete from favs
+            </button>
           </div>
         ) : (
           ""
