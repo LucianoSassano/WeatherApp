0f2e4302d7276ce18111ffb61b842076dfec7791
LucianoSassano <54543892+LucianoSassano@users.noreply.github.com>
2020-08-02 22:07:46 -0300

    shareFeatureDone

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = (props) => {
       <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
+            <li className="nav-item ">
               <a className="nav-link" href="./">
-                Home <span className="sr-only">(current)</span>
+                Home
               </a>
             </li>
             <a className="nav-link " href="./favorites">
